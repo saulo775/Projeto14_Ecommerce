7f3ddcb592fd975ac9064a6bca0f21e14cfb17a0
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-05-14 20:39:00 -0300

    Componentização do Header.

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -6,43 +6,6 @@ export const Container = styled.main`
     overflow-x: hidden;
 `;
 
-export const Header = styled.header`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 1.5rem 4rem;
-    background-color: var(--white);
-    color: var(--primary);
-
-    h1 {
-        font-family: 'Great Vibes', cursive;
-        font-size: 2.5rem;
-    }
-
-    div {
-        display: flex;
-        align-items: center;
-
-        nav {
-            display: flex;
-            gap: 2rem;
-            margin-right: 2rem;
-            font-size: 1.25rem;
-            transition: 300ms;
-        }
-    }
-
-    svg {
-        font-size: 1.8rem;
-        margin-left: 2rem;
-    }
-
-    svg:hover,
-    a:hover {
-        color: var(--secondary);
-    }
-`;
-
 export const Banner = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
